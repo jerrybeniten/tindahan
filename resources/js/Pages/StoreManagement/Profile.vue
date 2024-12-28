56c0ef0421ce113534a206fa9e54753cb19ce8bb
= <=>
2024-12-28 18:59:56 +0800

    resetForm

--- resources/js/Pages/StoreManagement/Profile.vue
+++ resources/js/Pages/StoreManagement/Profile.vue
@@ -26,7 +26,7 @@
                   class="btn btn-primary"
                   data-bs-toggle="modal"
                   data-bs-target="#createStoreLocation"
-                  @click.prevent="createStoreLocation"
+                  @click.prevent="resetForm()"
                 >
                   <i class="fas fa-plus"></i> Add Location
                 </button>
@@ -91,8 +91,8 @@
                     href="#"
                     class="text-decoration-none"
                     data-bs-toggle="modal"
-                    data-bs-target="#updateStore"
-                    @click.prevent="viewStore(storeLocations.uuid)"
+                    data-bs-target="#updateStoreLocation"
+                    @click.prevent="viewStoreLocation(storeLocations.uuid)"
                     ><i class="fas fa-regular fa-pen-to-square"></i></a
                 ></span>
                 <span
@@ -170,6 +170,281 @@
             <form v-if="!isSuccessfull" @submit.prevent="submitStoreLocation">
               <div class="modal-body">
                 <div class="row">
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label for="location_name" class="col-form-label"
+                        >Location Name:</label
+                      >
+                      <input
+                        v-model="location_name"
+                        type="text"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.location_name,
+                        }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.location_name }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="store-code" class="col-form-label">Store Code:</label>
+                      <input
+                        v-model="store_code"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.store_code }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.store_code }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="managers-name" class="col-form-label"
+                        >Managers Name:</label
+                      >
+                      <input
+                        v-model="manager_name"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.manager_name,
+                        }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.manager_name }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="email" class="col-form-label">Email:</label>
+                      <input
+                        v-model="email"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.email }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.email }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="phone" class="col-form-label">Phone:</label>
+                      <input
+                        v-model="phone"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.phone }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.phone }}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label for="addressLine1" class="col-form-label"
+                        >Address Line 1:</label
+                      >
+                      <input
+                        v-model="address_line1"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.address_line1,
+                        }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.address_line1 }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="addressLine2" class="col-form-label"
+                        >Address Line 2:</label
+                      >
+                      <input
+                        v-model="address_line2"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.address_line2,
+                        }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.address_line2 }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="city" class="col-form-label">City:</label>
+                      <input
+                        v-model="city"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.city }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.city }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="state" class="col-form-label">State:</label>
+                      <input
+                        v-model="state"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.state }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.state }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="zipCode" class="col-form-label">Zip Code:</label>
+                      <input
+                        v-model="zip_code"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.zip_code }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.zip_code }}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label for="country" class="col-form-label">Country:</label>
+                      <input
+                        v-model="country"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.country }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.country }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="latitude" class="col-form-label">Latitude:</label>
+                      <input
+                        v-model="latitude"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.latitude }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.latitude }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="longitude" class="col-form-label">Longitude:</label>
+                      <input
+                        v-model="longitude"
+                        class="form-control"
+                        :class="{ 'form-control': true, 'is-invalid': errors.longitude }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.longitude }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="longitude" class="col-form-label">Opening Hours:</label>
+                      <input
+                        v-model="opening_hours"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.opening_hours,
+                        }"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.opening_hours }}
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <div class="form-check form-switch">
+                        <input
+                          v-model="is_active"
+                          class="form-check-input"
+                          type="checkbox"
+                          role="switch"
+                          id="flexSwitchCheckDefault"
+                        />
+                        <label class="form-check-label" for="flexSwitchCheckDefault"
+                          >Enabled</label
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                  Close
+                </button>
+                <button
+                  type="submit"
+                  :class="isProcessing ? 'btn btn-primary disabled' : 'btn btn-primary'"
+                  :disabled="isProcessing"
+                >
+                  <div
+                    v-if="isProcessing"
+                    class="spinner-border spinner-border-sm me-2 mt-1"
+                    role="status"
+                  ></div>
+                  {{ isProcessing ? "Please Wait..." : "Submit" }}
+                </button>
+              </div>
+            </form>
+            <div v-else>
+              <div class="modal-body text-center">
+                <i class="fa-solid fa-check success-icon"></i>
+                <p class="success-text">Successful</p>
+              </div>
+              <!-- Success Notice -->
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--update store location modal-->
+      <div
+        class="modal fade"
+        id="updateStoreLocation"
+        tabindex="-1"
+        aria-labelledby="updateStoreLocationModalLabel"
+      >
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="updateStoreLocationModalLabel">
+                <i class="fas fa-regular fa-pen-to-square"></i> Update Store Location
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <form v-if="!isSuccessfull">
+              <div class="modal-body">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="mb-3">
+                      <label for="location_name" class="col-form-label">UUID:</label>
+                      <input
+                        v-model="uuid"
+                        type="text"
+                        class="form-control"
+                        :class="{
+                          'form-control': true,
+                          'is-invalid': errors.location_name,
+                        }"
+                        disabled="disabled"
+                      />
+                      <div class="invalid-feedback">
+                        {{ errors.location_name }}
+                      </div>
+                    </div>
+                  </div>
                   <div class="col-md-4">
                     <div class="mb-3">
                       <label for="location_name" class="col-form-label"
@@ -507,6 +782,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import { ref, reactive, computed, onMounted } from "vue";
 
+const uuid = ref("");
 const location_name = ref("");
 const store_code = ref("");
 const manager_name = ref("");
@@ -599,7 +875,22 @@ const viewStoreLocation = async (uuidData) => {
 
     if (response.status === 200) {
       viewData.value = response.data;
-      //uuid.value = viewData.value?.uuid;
+      uuid.value = viewData.value?.uuid;
+      location_name.value = viewData.value?.location_name;
+      store_code.value = viewData.value?.store_code;
+      manager_name.value = viewData.value?.manager_name;
+      email.value = viewData.value?.email;
+      phone.value = viewData.value?.phone;
+      address_line1.value = viewData.value?.address_line1;
+      address_line2.value = viewData.value?.address_line2;
+      city.value = viewData.value?.city;
+      state.value = viewData.value?.state;
+      zip_code.value = viewData.value?.zip_code;
+      country.value = viewData.value?.country;
+      latitude.value = viewData.value?.latitude;
+      longitude.value = viewData.value?.longitude;
+      is_active.value = viewData.value?.is_active == 1 ? true : false;
+      opening_hours.value = viewData.value?.opening_hours;
     }
   } catch (err) {
     // Handle error (invalid credentials, etc.)
@@ -610,6 +901,25 @@ const viewStoreLocation = async (uuidData) => {
       alert("An unexpected error occurred.");
     }
   }
+};
+
+const resetForm = (column) => {
+  uuid.value = "";
+  location_name.value = "";
+  store_code.value = "";
+  manager_name.value = "";
+  email.value = "";
+  phone.value = "";
+  address_line1.value = "";
+  address_line2.value = "";
+  city.value = "";
+  state.value = "";
+  zip_code.value = "";
+  country.value = "";
+  latitude.value = "";
+  longitude.value = "";
+  is_active.value = "";
+  opening_hours.value = "";
 };
 
 // getStores
